bf44e94b173ec31887520280564ba5b0bdad763c
Petr Vacula <vaculap@gmail.com>
2021-02-18 00:57:14 +0100

    refactoring - text correction in package.json - Python 3.5 requirement added to readme.md - variable names changed to snake_case - Robot Keyword change: Data Set Exists -> Fail If Data Set Exists - added header to .robot and .py files - method commentary changed to doc-string """ - double quotes replaced by single quotes - string concatenation done through %s now - command calls changed from os.system to subprocess.run -

--- frmwrk-robot/test/SORTSDK1.test.robot
+++ frmwrk-robot/test/SORTSDK1.test.robot
@@ -1,23 +1,25 @@
+"""
+    Sample test case for Robot testing framework.
+    author: Petr Vacula
+    license: MIT
+"""
+
 *** Settings ***
 Documentation    Mainframe Test Automation using Zowe Client SDK
 Library          mf_test_automation.py
 
 *** Variables ***
-${zosmfProfile}    TSO1
-${jcl}             test/SORTSDK1.jcl
-${testData}        test/SORTSDK1.input.data
-${expectedData}    test/SORTSDK1.expected.data
-${dsName}          PUBLIC.MFTSTAUT.ROBOT.SORTSDK1
+${zosmf_profile}    TSO1
+${jcl}              test/SORTSDK1.jcl
+${test_data}        test/SORTSDK1.input.data
+${expected_data}    test/SORTSDK1.expected.data
+${dataset_name}     PUBLIC.MFTSTAUT.ROBOT.SORTSDK1
 
 *** Test Cases ***
 Run SORT job and validate results
-    Data Set Exists    ${zosmfProfile}    ${dsName}
-    Create Data Set    ${zosmfProfile}    ${dsName}
-    Upload Test Data   ${zosmfProfile}    ${dsName}    ${testData}
-    ${jobResp}    Submit Jcl Notify  ${zosmfProfile}    ${jcl}
-    Validate Sort Job  ${zosmfProfile}    ${jobResp}    ${expectedData}
-    Delete Data Set    ${zosmfProfile}    ${dsName}
-
-
-
-
+    Fail If Data Set Exists  ${zosmf_profile}    ${dataset_name}
+    Create Data Set    ${zosmf_profile}    ${dataset_name}
+    Upload Test Data   ${zosmf_profile}    ${dataset_name}    ${test_data}
+    ${job_response}    Submit Jcl Notify  ${zosmf_profile}    ${jcl}
+    Validate Sort Job  ${zosmf_profile}    ${job_response}    ${expected_data}
+    Delete Data Set    ${zosmf_profile}    ${dataset_name}
--- frmwrk-robot/test/mf_test_automation.py
+++ frmwrk-robot/test/mf_test_automation.py
@@ -1,94 +1,111 @@
+"""
+    Sample test case for Robot testing framework.
+    author: Petr Vacula
+    license: MIT
+"""
+
 from zowe.zos_jobs_for_zowe_sdk import Jobs
 from zowe.zos_files_for_zowe_sdk import Files
-import os
 import time
 import subprocess
 import json
 
 
-# Check whether data set exists
-# Throw an exception if it does
-def data_set_exists(zosmfProfile, dsName):  # FIXME: more like "fail if data set exists"?
-    connection = { "plugin_profile": zosmfProfile}
+def fail_if_data_set_exists(zosmf_profile, dataset_name):
+    """
+    Check whether data set exists and throw and exception if it does
+    """
+    connection = {'plugin_profile': zosmf_profile}
     files = Files(connection)
-    lsDs = files.list_dsn(dsName)
+    list_dsn = files.list_dsn(dataset_name)
 
-    if lsDs['returnedRows'] == 0 :
+    if list_dsn['returnedRows'] == 0 :
         return False
-    elif lsDs['items'][0]['dsname'] == dsName :
-        raise AssertionError('>>> ERROR: ' + dsName + ' data set exists!')
+    elif list_dsn['items'][0]['dsname'] == dataset_name :
+        raise AssertionError('>>> ERROR: %s data set exists!' % (dataset_name))
     else:
         return False
 
 
-# Create a new test data set
-# Zowe Python SDK does not implement create data set yet -> using CLI command for now
-def create_data_set(zosmfProfile, dsName):
-    os.system('zowe zos-files create data-set-sequential ' + dsName + ' --zosmf-p ' + zosmfProfile)
+def create_data_set(zosmf_profile, dataset_name):
+    """
+    Create a new sequential data set
+    Zowe Python SDK does not implement create data set yet -> using CLI command for now
+    OPPORTUNITY: implement the data set create functionality under Python SDK
+    """
+    cmd = 'zowe zos-files create data-set-sequential %s --zosmf-p %s' % (dataset_name, zosmf_profile)
+    subprocess.run(cmd, capture_output=True, shell=True)
 
 
-# Upload test data into a newly created data set
-def upload_test_data(zosmfProfile, dsName, testData):
-    connection = { "plugin_profile": zosmfProfile}
+def upload_test_data(zosmf_profile, dataset_name, test_data):
+    """
+    Upload test data into a newly created data set
+    """
+    connection = {'plugin_profile': zosmf_profile}
     files = Files(connection)
-    files.upload_file_to_dsn(testData, dsName)
+    files.upload_file_to_dsn(test_data, dataset_name)
 
 
-# Submit job from local file
-# return job response
-def submit_jcl_notify(zosmfProfile, jcl):
+def submit_jcl_notify(zosmf_profile, jcl):
+    """
+    Submit job from local file
+    Return job response
+    """
     
     TIMEOUT = 120
     STEP = 2
 
-    connection = { "plugin_profile": zosmfProfile}
+    connection = {'plugin_profile': zosmf_profile}
     job = Jobs(connection)
-    resp = job.submit_from_local_file(jcl)
-    
-    jobName = resp["jobname"]
-    jobId = resp["jobid"]
+    response = job.submit_from_local_file(jcl)
+
+    job_name = response['jobname']
+    job_id = response['jobid']
 
     timeout = time.time() + TIMEOUT
-    while resp["status"] != 'OUTPUT':
+    while response['status'] != 'OUTPUT':
         if time.time() > timeout:
-            raise TimeoutError('>>> ERROR: Job %s(%s) exceeded timeout of %s secs' % (jobName, jobId, TIMEOUT))
+            raise TimeoutError('>>> ERROR: Job %s(%s) exceeded timeout of %s secs' % (job_name, job_id, TIMEOUT))
         time.sleep(STEP)
-        resp = job.get_job_status(jobName, jobId)    
+        response = job.get_job_status(job_name, job_id)    
     
-    return resp
+    return response
 
 
-# Run validation for sort job
-# check RC=0 and SORTOUT is as expected
-def validate_sort_job(zosmfProfile, jobResp, expectedData):
-    if jobResp["retcode"] != 'CC 0000':
-        raise AssertionError('>>> ERROR: ' + jobResp["jobname"] + '(' + jobResp["jobid"] + 
-    ') ended with ' + jobResp["retcode"])
+def validate_sort_job(zosmf_profile, job_response, expected_data):
+    """
+    Run validation for sort job
+    check RC=0 and SORTOUT is as expected
+    """
+    if job_response['retcode'] != 'CC 0000':
+        raise AssertionError('>>> ERROR: %s(%s) ended with %s' 
+    % (job_response['jobname'], job_response['jobid'], job_response['retcode']))
 
-    jobId = jobResp["jobid"]
+    job_id = job_response['jobid']
     
-    cmd = ['zowe', 'zos-jobs', 'list', 'spool-files-by-jobid', jobId, '--rfj', '--zosmf-p', zosmfProfile]
-    p = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.STDOUT, shell=True)
-    stdout,stderr = p.communicate()
-    outJson = json.loads(stdout)
+    cmd = 'zowe zos-jobs list spool-files-by-jobid %s --rfj --zosmf-p %s' % (job_id, zosmf_profile)
+    result = subprocess.run(cmd, capture_output=True, shell=True)
+    output_json = json.loads(result.stdout)
 
-    ddId = 0
-    for item in outJson['data']:
-        if item["ddname"] == 'SORTOUT':
-            ddId = item["id"]
+    dd_id = 0
+    for item in output_json['data']:
+        if item['ddname'] == 'SORTOUT':
+            dd_id = item['id']
             break
     
-    cmd = ['zowe', 'zos-jobs', 'view', 'spool-file-by-id', jobId, str(ddId), '--rfj', '--zosmf-p', zosmfProfile]
-    p = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.STDOUT, shell=True)
-    stdout,stderr = p.communicate()
-    outJson = json.loads(stdout)
+    cmd = 'zowe zos-jobs view spool-file-by-id %s %s --rfj --zosmf-p %s' % (job_id, dd_id, zosmf_profile)
+    result = subprocess.run(cmd, capture_output=True, shell=True)
+    output_json = json.loads(result.stdout)
 
-    if outJson['stdout'] != open(expectedData).read():
-        raise AssertionError(">>> ERROR: SORTOUT output is not as expected")
+    if output_json['stdout'] != open(expected_data).read():
+        raise AssertionError('>>> ERROR: SORTOUT output is not as expected')
 
 
-# Delete test data set
-# Zowe Python SDK does not implement delete data set yet -> using CLI command for now
-def delete_data_set(zosmfProfile, dsName):
-    os.system('zowe zos-files delete data-set ' + dsName + ' --for-sure --zosmf-p ' + zosmfProfile)
-    return True
+def delete_data_set(zosmf_profile, dataset_name):
+    """
+    Delete test data set
+    Zowe Python SDK does not implement delete data set yet -> using CLI command for now
+    OPPORTUNITY: implement the data set create functionality under Python SDK
+    """
+    cmd = 'zowe zos-files delete data-set %s --for-sure --zosmf-p %s' % (dataset_name, zosmf_profile)
+    subprocess.run(cmd, capture_output=True, shell=True)
